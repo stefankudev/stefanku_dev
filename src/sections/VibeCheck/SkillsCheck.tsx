910f15ead5ddf582f85de52df8b1dd7d2ed89aa3
Stefan Kudev <stef@nku.dev>
2026-03-26 17:28:27 +0000

    chore: update SkillsCheck

--- src/sections/VibeCheck/SkillsCheck.tsx
+++ src/sections/VibeCheck/SkillsCheck.tsx
@@ -1,16 +1,21 @@
 import Card from '../../components/Card/Card'
 import cn from 'classnames'
-import css from './VibeCheck.module.css'
+import css from './SkillsCheck.module.css'
 
 export default function SkillsCheck() {
   return (
     <Card>
       <h3>
-        Skills check <span id={cn(css.vibeCheckEmoji)}>👇</span>
+        Skills check <span id={cn(css.skillsCheckEmoji)}>👇</span>
       </h3>
       <hr className={cn(css.fancyHr)} />
       <Card>
-        <p>Stuff goes here...</p>
+        <p>**Add:**
+          - TypeScript, AWS, CDK, Terraform (with logos and perfect accessibility)
+          - AWS DVA-C02 certification badge
+          - GitHub link
+          - Skills categories: Frontend, Backend, Cloud/DevOps
+          - "Specialties" section: "Cloud Architecture", "Financial Services", "TDD"</p>
       </Card>
     </Card>
   )
