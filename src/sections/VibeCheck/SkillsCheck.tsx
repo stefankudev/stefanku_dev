40593318cf5c38b7d4a44217fd8596a1dbdefafb
Stefan Kudev <stef@nku.dev>
2026-03-26 17:14:50 +0000

    chore: tidy up SkillsCheck component

--- src/sections/VibeCheck/SkillsCheck.tsx
+++ src/sections/VibeCheck/SkillsCheck.tsx
@@ -2,11 +2,7 @@ import Card from '../../components/Card/Card'
 import cn from 'classnames'
 import css from './VibeCheck.module.css'
 
-interface SkillsCheckProps {
-  codeWarsBadgeSize?: string
-}
-
-export default function SkillsCheck({ codeWarsBadgeSize = 'large' }: SkillsCheckProps) {
+export default function SkillsCheck() {
   return (
     <Card>
       <h3>
@@ -14,20 +10,7 @@ export default function SkillsCheck({ codeWarsBadgeSize = 'large' }: SkillsCheck
       </h3>
       <hr className={cn(css.fancyHr)} />
       <Card>
-        <div className={cn(css.imageWrapper)}>
-          <a href="https://www.codewars.com/users/stefan_kudev" target="_blank" rel="noreferrer">
-            <img
-              id={css.codewarsImgEmbed}
-              src={'https://www.codewars.com/users/stefan_kudev/badges/' + codeWarsBadgeSize}
-              alt="Stefan Kudev's score on Codewars"
-            />
-          </a>
-          <img
-            id={css.psIQimageEmbed}
-            src="/pluralsight_iq_js_core_language_259.png"
-            alt="Stefan Kudev's JavaScript Core Language PluralSight IQ"
-          />
-        </div>
+        <p>Stuff goes here...</p>
       </Card>
     </Card>
   )
