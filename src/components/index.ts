74b463353141e75ce3d146faa96c566d32b82099
Stefan Kudev <stef@nku.dev>
2026-03-28 18:21:57 +0000

    refactor: ...but what if it was written in VITE?

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,5 @@
 export { default as Button } from './Button/Button';
 export { default as Card } from './Card/Card';
-export { default as ErrorBoundary } from './ErrorBoundary/ErrorBoundary';
 export { default as ListItemCard } from './ListItemCard/ListItemCard';
-export { default as ProjectCard } from './ProjectCard/ProjectCard';
 export { default as TestimonialCard } from './TestimonialCard/TestimonialCard';
 export { default as UnderConstruction } from './UnderConstruction/UnderConstruction';
--- src/sections/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as CommunityWork, Advocacy, Mentorship } from './CommunityWork/CommunityWork';
-export { default as ContactDetails } from './ContactDetails/ContactDetails';
-export { default as Hobbies } from './Hobbies/Hobbies';
-export { default as Projects } from './Projects/Projects';
-export { default as Testimonials } from './Testimonials/Testimonials';
-export { default as TitleCard } from './TitleCard/TitleCard';
