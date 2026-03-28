1ec84358f448e4d54d9ec880f0edf37a491f9082
Stefan Kudev <stef@nku.dev>
2026-03-28 18:36:53 +0000

    style: fix styles

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Button } from './Button/Button';
-export { default as Card } from './Card/Card';
-export { default as ListItemCard } from './ListItemCard/ListItemCard';
-export { default as TestimonialCard } from './TestimonialCard/TestimonialCard';
-export { default as UnderConstruction } from './UnderConstruction/UnderConstruction';
