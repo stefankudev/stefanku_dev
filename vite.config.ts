a0882ca8090b6f0bbb68be0d3644dea9f320935f
Stefan Kudev <stef@nku.dev>
2026-03-28 16:53:16 +0000

    feat: image optimization

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import fs from 'fs';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   plugins: [
     react(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+      webp: {
+        quality: 80,
+      },
+    }),
     {
       name: 'exclude-psd-from-public',
       closeBundle() {
